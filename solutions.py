469b75bd56beca5c32274680e1b0443c499fb837
yongqingshannnn <yongqing.shann@gmail.com>
2024-05-29 22:36:05 +0800

    Made change!

--- solutions.py
+++ solutions.py
@@ -11,6 +11,10 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+total_sales = float(input("What is projected amount of total sales?"))
+x = total_sales * float(0.23)
+output_message = "\"profit: $" + str(x) + "\" for sales of $" + str(total_sales) + ","
+print(output_message)
 
 
 def calculate_quotient_and_remainder():
@@ -22,7 +26,12 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
-
+first_number = int(input("enter number #1"))
+second_number = int(input("enter number #2"))
+x = first_number//second_number
+y = first_number % second_number
+output_message = str(second_number) + " goes into " + str(first_number) + " a total of " + str(x) + " times with a remainder of " + str(y)
+print(output_message)
 
 def calculate_miles_per_gallon():
     """
@@ -35,7 +44,11 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
-
+number_of_miles = int(input("How many miles you drive today?"))
+gallons_of_gas = int(input("How many gallons of gas you used today?"))
+x = number_of_miles / gallons_of_gas
+output_message = "Miles diven: " + str(number_of_miles) + "\nGas used (gallons): " + str(gallons_of_gas) + "\nMiles per gallon: " + str(x)
+print(output_message)
 
 def align_text():
     """
@@ -53,3 +66,11 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+price_one = float(input("Enter price #1"))
+price_two = float(input("Enter price #2"))
+price_three = float(input("Enter price #3"))
+x = format(price_one, '.2f')
+y = format(price_two, '.2f')
+z = format(price_three, '.2f')
+output_message = "Price #1: $    " + str(x) + "\nPrice #2: $   " + str(y) + "\nPrice #3: $ " + str(z)
+print(output_message)
